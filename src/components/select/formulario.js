5d158ebea2ef6d5a606390c47e0295c143b55486
SunlightShield <sespinozahenriquez@gmail.com>
2023-07-05 16:31:38 -0400

    ajustes

--- src/components/select/formulario.js
+++ src/components/select/formulario.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import MainCard from 'components/MainCard';
-import { Typography } from '@mui/material';
+import { Typography, Box, TextField, Button } from '@mui/material';
 // import { useState } from 'react';
 // import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 // import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
@@ -9,18 +9,53 @@ import { Typography } from '@mui/material';
 
 export default function Formulario() {
 
+  const Guardar = async () => {
+
+    const nombre = document.getElementById('nombre').value
+    const correo = document.getElementById('correo').value
+    const telefono = document.getElementById('telefono').value
+    const rut = document.getElementById('rut').value
+    const comentario = document.getElementById('comentario').value
+
+    if(nombre === '' || correo === '' || telefono === '' || rut === '' || comentario === ''){
+
+      console.log('rellena los campos por favor')
+    }else{
+      console.log('campos listos')
+    }
+
+
+  }
+
+
 
   return (
-    <MainCard title="Formulario" sx={{ height: '100%' }}>
+    <MainCard title="Agendar Entrevista (logo)" sx={{ height: '100%' }}>
 
-  <Typography variant="h6">Formulario</Typography>
+      <Typography variant="h6">Introduzca los detalles</Typography>
+      {/* <Typography variant="h6">Espacio para la data que me llega desde los demas componentes</Typography> */}
 
-  <Typography variant="caption" color="textSecondary">
+      <Box sx={{ maxWidth: 500}} >
 
-    Este es tu formulario
+      <TextField id="nombre" label="Nombre" fullWidth variant="standard"  margin="dense"/> 
 
-  </Typography>
+      <TextField id="correo" label="Correo Electronico" fullWidth variant="standard" margin="dense" />
 
-</MainCard>
+      <TextField id="telefono" label="Telefono" fullWidth variant="standard"   margin="dense"/>
+
+      <TextField id="rut" label="Rut" fullWidth variant="standard"  margin="dense" />
+
+      <TextField id="comentario" fullWidth label="Comentario"  margin="dense"
+      multiline rows={5} />
+
+
+      <Button variant="contained" color="error" margin="dense" onClick={() => Guardar()}> 
+      {/* onClick={(e) => Guardar() */}
+        Programar Evento
+      </Button>
+
+      </Box>
+
+    </MainCard>
   );
 }
